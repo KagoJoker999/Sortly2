d4a9f18af38a70696c2e8d36af99d76b6af447f9
KagoJournal <majingkaihk@163.com>
2025-01-07 15:28:25 +0800

    Update: 版本号更新为2.1.0

--- version.js
+++ version.js
@@ -1,9 +1,9 @@
-const APP_VERSION = 'v2.0.0';
+const version = '2.1.0';
 
 // 在页面加载时设置版本号
 document.addEventListener('DOMContentLoaded', () => {
     const versionElements = document.getElementsByClassName('version-tag');
     for (const element of versionElements) {
-        element.textContent = APP_VERSION;
+        element.textContent = version;
     }
 }); 
